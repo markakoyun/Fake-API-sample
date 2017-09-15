7cd92853ce4d217af669591fe043da28074d8020
markakoyun <markdjaime@gmail.com>
2017-09-15 12:10:01 +0800

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    var $sample = $('.row');
+    var $load = $('#load');
+
+    $load.on('click', function () {
+        $.ajax({
+            dataType: "json",
+            type: 'GET',
+            url: 'js/sample.json',
+            success: success
+        });
+
+        function success(data) {
+            var result = "";
+            $.each(data, function (i, data) {
+                result += "<div class='col-md-3'>";
+                result += "<div class='thumbnail text-center'>";
+                result += "<p>Name :" + data.name + "</p>";
+                result += "<p>Username :" + data.username + "</p>";
+                result += "<p>Email :" + data.email + "</p>";
+                result += "<p>Adress :" + data.address.street + "</p>";
+                result += "<p>Phone :" + data.phone + "</p>";
+                result += "<p>Website :" + data.website + "</p>";
+                result += "<p>Company :" + data.company + "</p>";
+                result += "</div>"
+                result += "</div>"
+            });
+
+            $sample.html(result);
+        }
+    });
+
+
+
+});
